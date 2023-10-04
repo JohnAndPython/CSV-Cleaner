c046a63a266dd456bd557f07234e6d814d7cbb0d
JohnAndPython <146953522+JohnAndPython@users.noreply.github.com>
2023-10-04 14:49:23 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,30 @@
+#********************#
+# Main Programm
+#********************#
 
+
+import os
+import csv
+
+path_of_main = os.path.dirname(os.path.abspath(__file__))
+
+#Create necessary folders, if they do not exist
+try:
+    os.mkdir(path_of_main + "\\Template")
+except FileExistsError:
+    None
+
+try:
+    os.mkdir(path_of_main + "\\Erorrprotokoll")
+except FileExistsError:
+    None
+
+try:
+    os.mkdir(path_of_main + "\\Offers")
+except FileExistsError:
+    None
+
+try:
+    os.mkdir(path_of_main + "\\Cleaned")
+except FileExistsError:
+    None
